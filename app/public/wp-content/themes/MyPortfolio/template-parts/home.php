83a3a75689547231e0ca003bcc83fb9958bc4534
Barasa Ashina <ashina.barasa@thejitu.com>
2023-10-11 20:22:56 +0300

    first commit

---
+++ app/public/wp-content/themes/MyPortfolio/template-parts/home.php
@@ -0,0 +1,194 @@
+<?php
+
+/**
+ * The template for displaying the Home page
+ *  
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ * 
+ *  Template Name: Home Page
+ * @package myPortfolio
+ */
+
+// get_header();
+
+?>
+
+ <!-- Banner -->
+ <section class="home-banner">
+            <div class="banner">
+                <div class="banner-text">
+                    <h1>Lorem ipsum dolor sit amet</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                        incididunt ut labore et dolore magna aliqua.</p>
+                    <div class="buttons">
+                        <button>Hire Me</button>
+                        <button>Download My CV</button>
+                    </div>
+                </div>
+                <div class="banner-image">
+                    <img class="image" src="C:\Users\AshinaBarasa\Desktop\My projects\My Portfolio\assets\images\image placeholder.jfif">
+                </div>
+            </div>
+            <div class="shadow">
+                
+            </div>
+        </section>
+
+        <section class="about-me">
+            <div class="about">
+                <div class="left">
+                    <img class="about-image" src="C:\Users\AshinaBarasa\Desktop\My projects\My Portfolio\assets\images\image placeholder.jfif">
+                </div>
+                <div class="right">
+                    <div class="text">
+                        <h2>About Me</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                        incididunt ut labore et dolore magna aliqua.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                        incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                    <div class="awards">
+                        <div class="award">
+                            <img class="award-image" src="C:\Users\AshinaBarasa\Desktop\My projects\My Portfolio\assets\images\work.png">
+                            <p class="award-text">1+ Years of<br>Experience</p>
+                        </div>
+                        <div class="award">
+                            <img class="award-image" src="C:\Users\AshinaBarasa\Desktop\My projects\My Portfolio\assets\images\software-development.png">
+                            <p class="award-text">5+ Projects</p>
+                        </div>
+                    </div>
+                </div>
+                </div>
+        </section>
+
+        <section class="my-services">
+            <h2>My Services</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+            incididunt ut labore et dolore magna aliqua.</p>
+            <div class="services">
+                <div class="service">
+                    <img class="service-image" src="C:\Users\AshinaBarasa\Desktop\My projects\My Portfolio\assets\images\web dev.jpg">
+                    <h4>Website Development</h4>
+                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                     incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+                <div class="service">
+                    <img class="service-image" src="C:\Users\AshinaBarasa\Desktop\My projects\My Portfolio\assets\images\mobile dev.jpg">
+                    <h4>Mobile App Development</h4>
+                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                     incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+                <div class="service">
+                    <img class="service-image" src="C:\Users\AshinaBarasa\Desktop\My projects\My Portfolio\assets\images\PM.jpg">
+                    <h4>Project Management</h4>
+                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                     incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+            </div>
+        </section>
+
+        <section class="my-tech">
+            <div class="tech-container">
+                <div class="tech-description">
+                    <h2>My Technologies</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                        incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+                <div class="grid-tech">
+                    <div class="tech-item">
+                        <img class="tech-image" src="C:\Users\AshinaBarasa\Desktop\My projects\My Portfolio\assets\images\c.png">
+                        <div class="tech-text">
+                            <p>C</p>
+                        </div>
+                    </div>
+                    <div class="tech-item">
+                        <img class="tech-image" src="C:\Users\AshinaBarasa\Desktop\My projects\My Portfolio\assets\images\c++.png">
+                        <div class="tech-text">
+                            <p>C++</p>
+                        </div>
+                    </div>
+                    <div class="tech-item">
+                        <img class="tech-image" src="C:\Users\AshinaBarasa\Desktop\My projects\My Portfolio\assets\images\java.png">
+                        <div class="tech-text">
+                            <p>Java</p>
+                        </div>
+                    </div>
+                    <div class="tech-item">
+                        <img class="tech-image" src="C:\Users\AshinaBarasa\Desktop\My projects\My Portfolio\assets\images\python.png">
+                        <div class="tech-text">
+                            <p>Python</p>
+                        </div>
+                    </div>
+                    <div class="tech-item">
+                        <img class="tech-image" src="C:\Users\AshinaBarasa\Desktop\My projects\My Portfolio\assets\images\js.png">
+                        <div class="tech-text">
+                            <p>JavaScript</p>
+                        </div>
+                    </div>
+                    <div class="tech-item">
+                        <img class="tech-image" src="C:\Users\AshinaBarasa\Desktop\My projects\My Portfolio\assets\images\ts.png">
+                        <div class="tech-text">
+                            <p>TypeScript</p>
+                        </div>
+                    </div>
+                    <div class="tech-item">
+                        <img class="tech-image" src="C:\Users\AshinaBarasa\Desktop\My projects\My Portfolio\assets\images\angular.png">
+                        <div class="tech-text">
+                            <p>Angular</p>
+                        </div>
+                    </div>
+                    <div class="tech-item">
+                        <img class="tech-image" src="C:\Users\AshinaBarasa\Desktop\My projects\My Portfolio\assets\images\wp.png">
+                        <div class="tech-text">
+                            <p>WordPress</p>
+                        </div>
+                    </div>
+                    <div class="tech-item">
+                        <img class="tech-image" src="C:\Users\AshinaBarasa\Desktop\My projects\My Portfolio\assets\images\cypress.png">
+                        <div class="tech-text">
+                            <p>Cypress</p>
+                        </div>
+                    </div>
+                    <div class="tech-item">
+                        <img class="tech-image" src="C:\Users\AshinaBarasa\Desktop\My projects\My Portfolio\assets\images\MySQL.png">
+                        <div class="tech-text">
+                            <p>MySQL</p>
+                        </div>
+                    </div>
+                    <div class="tech-item">
+                        <img class="tech-image" src="C:\Users\AshinaBarasa\Desktop\My projects\My Portfolio\assets\images\mssql.png">
+                        <div class="tech-text">
+                            <p>MSSQL</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="Contact-me">
+            <div class="contact-container">
+                <div class="contact-text">
+                    <h2>Contact Me</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                        incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+                <div class="contact-form">
+                    <form>
+                        <input type="text" id="name" name="name" placeholder="Name*">
+                        <input type="email" id="email" name="email" placeholder="Email Address*">
+                        <input type="tel" id="phone-number" name="phone-number" placeholder="Phone Number">
+                        <textarea id="message" name="message" placeholder="message"></textarea>
+                        <input type="submit">
+                    </form>
+                </div>
+            </div>
+        </section>
+
+        <section class="footer">
+            <div class="footer-container">
+                <div class="footer-column">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                        incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+            </div>
+        </section>
+
